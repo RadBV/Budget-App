42f7de1cc0c7f93d25f6254b261611932eba0363
Radharani Valongo <radha.valongo@gmail.com>
2026-05-13 14:07:19 -0400

    add functionality to displayCategory method
    separate header logic into determineHeader method
    
    create displayLedgerEntry method to format and print each entry individually using loop in displayCategory

--- budgetApp.py
+++ budgetApp.py
@@ -10,9 +10,12 @@ class Category:
             return False
         return True
     
+    def getBalance(self):
+        return self.balance
+    
     def deposit(self, amount, description=""):
         print(f"Depositing into {self.name}...")
-        self.ledger.append({'amount': amount, 'description': description})
+        self.ledger.append({'amount': format(amount, ".2f"), 'description': description})
         self.balance += amount
         print("Deposit Successful!")
         
@@ -21,7 +24,7 @@ class Category:
         if not self.checkFunds(amount):
             print("Declined: Not enough for withdrawal.")
             return False
-        self.ledger.append({'amount': -amount, 'description': description})
+        self.ledger.append({'amount': format(-amount,".2f"), 'description': description})
         self.balance -= amount
         print("Withdrawal Successful!")
         return True
@@ -35,22 +38,33 @@ class Category:
         category.deposit(amount, f"Transfer from {self.name}")
         print("Transfer Successful!")
         return True
-    
-    def getBalance(self):
-        return f"Current Balance: ${self.balance}"
+        
     
     def displayCategory(self):
+        header = self.determineHeader()
+        print(header)
+        
+        for entry in list(self.ledger):
+            self.printLedgerEntry(entry)
+            
+        print("Total: " + format(self.balance, ".2f"))
+    
+    def determineHeader(self):
         fullAsterisks = int(30 - len(self.name))
         halfAsterisks = "*" * int(fullAsterisks/2)
         
-        header = ""
-        
         if fullAsterisks/2 % 2 == 0.5 or fullAsterisks/2 % 2 == 1.5:
-            header = halfAsterisks + self.name + halfAsterisks + "*"
+            return halfAsterisks + self.name + halfAsterisks + "*"
         else:
-            header = halfAsterisks + self.name + halfAsterisks 
-
-        print(header)
+            return halfAsterisks + self.name + halfAsterisks 
+        
+            
+    def printLedgerEntry(self, ledgerEntry):
+        joinedDescriptionAndAmount = ledgerEntry['description'][0:22] + ledgerEntry['amount']
+        spaceBetween = " " * (30 - len(joinedDescriptionAndAmount))
+        
+        print(ledgerEntry['description'][0:22] + spaceBetween + ledgerEntry['amount'])
+        return
 
 def createSpendChart(categories):
     pass
